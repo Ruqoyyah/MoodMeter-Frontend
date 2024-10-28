9fa928b683ccf768536c46daef816468501d54bd
Quadds <akinpelumoyo@gmail.com>
2024-10-28 20:43:24 +0100

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 
 export default function Home() {
-  type Mood = "Relaxed" | "Anxious" | "Happy" | "Angry" | "Sad";
+  type Mood = "RELAXED" | "ANXIOUS" | "HAPPY" | "ANGRY" | "SAD";
 
   const MoodSelector: React.FC = () => {
     const [selectedMood, setSelectedMood] = useState<Mood | null>(null);
@@ -42,36 +42,30 @@ export default function Home() {
     }, []);
 
     const moodContent: Record<Mood, { question: string; scale: number }> = {
-      Anxious: { question: "How anxious are you?", scale: 10 },
-      Relaxed: { question: "How relaxed are you?", scale: 10 },
-      Happy: { question: "How happy are you?", scale: 10 },
-      Sad: { question: "How sad are you?", scale: 10 },
-      Angry: { question: "How angry are you?", scale: 10 },
+      ANXIOUS: { question: "How anxious are you?", scale: 10 },
+      RELAXED: { question: "How relaxed are you?", scale: 10 },
+      HAPPY: { question: "How happy are you?", scale: 10 },
+      SAD: { question: "How sad are you?", scale: 10 },
+      ANGRY: { question: "How angry are you?", scale: 10 },
     };
 
     // Handle the display of the first toast notification
     const handleMoodSelection = async (mood: Mood) => {
       setSelectedMood(mood);
+      setShowScale(true);
 
       try {
         await axios.post("http://localhost:8081/api/mood/create-mood", {
           rating: mood,
         });
+        router.push("/success");
       } catch (error) {
         console.log(error);
       }
     };
 
     // Handle the final submission of the text box
-    const handleFinalSubmit = () => {
-      toast("Your details have been submitted successfully!", {
-        onClose: () => {
-          // Refresh the page after the second toast ends
-          window.location.reload();
-        },
-        autoClose: 2000, // Optional: Adjust the duration of the toast
-      });
-    };
+    const handleFinalSubmit = () => {};
 
     return (
       <>
@@ -83,7 +77,7 @@ export default function Home() {
             className={`p-5 rounded-xl relative z-10 flex flex-col gap-8 items-center justify-center bg-[#00000040]`}
           >
             {/* Header Text */}
-            <p className="font-zubajda sm:text-[62px] sm:leading-[80px] text-xl font-bold text-white">
+            <p className="font-zubajda sm:text-[62px] text-xl sm:leading-[80px] font-bold text-white">
               Hello, <br />
               what is your cherry republic mood today?
             </p>
@@ -94,7 +88,7 @@ export default function Home() {
                 {/* Anxious Button */}
                 <div className="flex flex-col items-center ">
                   <button
-                    onClick={() => handleMoodSelection("Anxious")}
+                    onClick={() => handleMoodSelection("ANXIOUS")}
                     className="ease-in-out duration-700 hover:bg-white focus:outline-none bg-[#ffffff80] rounded-full p-2"
                   >
                     <Image
@@ -109,7 +103,7 @@ export default function Home() {
                 {/* Relaxed Button */}
                 <div className="flex flex-col items-center ">
                   <button
-                    onClick={() => handleMoodSelection("Relaxed")}
+                    onClick={() => handleMoodSelection("RELAXED")}
                     className="ease-in-out duration-700 hover:bg-white focus:outline-none bg-[#ffffff80] rounded-full p-2"
                   >
                     <Image
@@ -124,7 +118,7 @@ export default function Home() {
                 {/* Happy Button */}
                 <div className="flex flex-col items-center ">
                   <button
-                    onClick={() => handleMoodSelection("Happy")}
+                    onClick={() => handleMoodSelection("HAPPY")}
                     className="ease-in-out duration-700 hover:bg-white focus:outline-none bg-[#ffffff80] rounded-full p-2"
                   >
                     <Image src={Happy} alt="Happy Icon" className="w-32 h-32" />
@@ -135,7 +129,7 @@ export default function Home() {
                 {/* Sad Button */}
                 <div className="flex flex-col items-center ">
                   <button
-                    onClick={() => handleMoodSelection("Sad")}
+                    onClick={() => handleMoodSelection("SAD")}
                     className="ease-in-out duration-700 hover:bg-white focus:outline-none bg-[#ffffff80] rounded-full p-2"
                   >
                     <Image src={Sad} alt="Sad Icon" className="w-32 h-32" />
@@ -146,7 +140,7 @@ export default function Home() {
                 {/* Angry Button */}
                 <div className="flex flex-col items-center ">
                   <button
-                    onClick={() => handleMoodSelection("Angry")}
+                    onClick={() => handleMoodSelection("ANGRY")}
                     className="ease-in-out duration-700 hover:bg-white focus:outline-none bg-[#ffffff80] rounded-full p-2"
                   >
                     <Image src={Angry} alt="Angry Icon" className="w-32 h-32" />
@@ -174,7 +168,7 @@ export default function Home() {
                           }`}
                           onClick={() => {
                             setSelectedScale(i + 1);
-                            setShowTextBox(true); // Show the text box after selecting scale
+                            // setShowTextBox(true); // Show the text box after selecting scale
                           }}
                         >
                           {i + 1}
@@ -202,14 +196,14 @@ export default function Home() {
                   ></textarea>
 
                   {/* Enter Button */}
-                  <button
-                    className="mt-4 bg-cyan-300 text-white px-6 py-2 rounded-lg hover:bg-purple-100 focus:outline-none"
-                    onClick={handleFinalSubmit}
-                  >
-                    Enter
-                  </button>
                 </div>
               )}
+              {/* <button
+                className="mt-4 bg-cyan-300 text-white px-6 py-2 rounded-lg hover:bg-purple-100 focus:outline-none"
+                onClick={handleFinalSubmit}
+              >
+                Enter
+              </button> */}
             </div>
           </div>
         </main>
